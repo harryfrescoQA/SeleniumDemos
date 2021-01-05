0331d01b1293325e2dd533e4f8a798b507888122
harryfresco <harry.fresco@students.plymouth.ac.uk>
2021-01-05 14:46:59 +0000

    HF: Attempting extent reports

--- seleniumTest/src/test/java/seleniumTest/DemoSiteTest.java
+++ seleniumTest/src/test/java/seleniumTest/DemoSiteTest.java
@@ -15,6 +15,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class DemoSiteTest {
 	 private WebDriver driver;
 
@@ -23,11 +27,20 @@ public class DemoSiteTest {
 	        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
 	        driver = new ChromeDriver();
 	        driver.manage().window().setSize(new Dimension(1366, 768));
+	        
+
+
 
 	    }
 
 	    @Test
 	    public void test() throws InterruptedException {
+	    	
+	        ExtentReports extent = new ExtentReports("C:\\Users\\harry\\Documents\\SeleniumTests\\seleniumTest\\src\\test\\resources", true); 
+	        ExtentTest test;
+	        
+	        test = extent.startTest("Checking search results");
+	        
 	    	 driver.get("http://automationpractice.com/index.php");
 	         WebElement searchBar = driver.findElement(By.id("search_query_top"));
 	         String searchTerm = "blouse";
@@ -37,7 +50,7 @@ public class DemoSiteTest {
 	      WebElement results = driver.findElement(By.cssSelector("#center_column > ul > li > div > div.right-block > h5 > a"));
 
 	      assertEquals("Blouse", results.getText());
-
+	        test.log(LogStatus.PASS, results.getText());
 	    }
 
 	    @After
