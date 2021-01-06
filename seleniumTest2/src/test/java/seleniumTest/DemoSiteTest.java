80900895c4439dd7aee8468b6cf61eeeb5a6bde1
harryfresco <harry.fresco@students.plymouth.ac.uk>
2021-01-06 11:53:40 +0000

    HF: updated demo site test with cucumber

--- seleniumTest2/src/test/java/seleniumTest/DemoSiteTest.java
+++ seleniumTest2/src/test/java/seleniumTest/DemoSiteTest.java
@@ -5,49 +5,26 @@ import static org.junit.Assert.assertEquals;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 import pages.createUser;
 import pages.landingPage;
 import pages.login;
-
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/cuke/DemoSite.feature", monochrome=true,glue = {"stepdef"})
 public class DemoSiteTest {
-	 private WebDriver driver;
 
-	    @Before
-	    public void setup() {
-	        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-	        driver = new ChromeDriver();
-	        driver.manage().window().setSize(new Dimension(1366, 768));
 
-	    }
+	 
 
-	    @Test
-	    public void test() throws InterruptedException {
-	        driver.get(landingPage.URL);
-	        landingPage landingPage = new landingPage(driver);
-	        
-	        landingPage.clickCreate();
 
-	        
 
-	        createUser createUser = new createUser(driver);
-	        createUser.createForm();
 
-	        createUser.login();
-
-	        login login = new login(driver);
-	        login.login();
-	        
-	       assertEquals("**Successful Login**", login.checkLogin());
-	    } 
-
-	    @After
-	    public void tearDown() {
-	        driver.close();
-	    }
 }
