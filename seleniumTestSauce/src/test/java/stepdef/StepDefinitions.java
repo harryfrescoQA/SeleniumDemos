e35e1a34bf6024d7f4fab2e40e9f732931a6ce24
harryfresco <harry.fresco@students.plymouth.ac.uk>
2021-01-06 14:25:20 +0000

    HF: Added Cucumber to Sauce Shop

--- seleniumTestSauce/src/test/java/stepdef/StepDefinitions.java
+++ seleniumTestSauce/src/test/java/stepdef/StepDefinitions.java
@@ -1,41 +1,80 @@
 package stepdef;
 
-import org.openqa.selenium.WebDriver;
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import pages.inventoryPage;
+import pages.landingPage;
 
 public class StepDefinitions {
-	
+	private WebDriver driver;
+	 List<WebElement> results;
+	 List<String> names;
+	@Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1366, 768));
+
+    }
 	@Given("^the correct web address$")
-	public void the_correct_web_address() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void the_correct_web_address() {
+		  driver.get(landingPage.URL);
 	}
 
 	@When("^I enter the correct credentials$")
-	public void i_enter_the_correct_credentials() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void i_enter_the_correct_credentials() {
+		 landingPage landingPage = new landingPage(driver);
+        landingPage.login("standard_user", "secret_sauce");
 	}
 
 	@Then("^I can see the inventory page$")
 	public void i_can_see_the_inventory_page() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	    assertEquals("https://www.saucedemo.com/inventory.html",driver.getCurrentUrl());
 	}
 
 	@When("^I log in and add all to cart$")
 	public void i_log_in_and_add_all_to_cart() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+		landingPage landingPage = new landingPage(driver);
+        landingPage.login("standard_user", "secret_sauce");
+        results = driver.findElements(By.className("inventory_item"));
+   	    names = new ArrayList();
+   	 for(WebElement product : results) {
+   		 //product = driver.findElement(By.className("inventory_item"));
+   		 WebElement buttondiv = product.findElement(By.className("pricebar"));
+   		 WebElement buttoncart = buttondiv.findElement(By.tagName("button"));
+   		 buttoncart.click();
+   		 names.add(product.findElement(By.className("inventory_item_name")).getText());
+   	 }
 	}
 
 	@Then("^I can go to checkout and see all items$")
-	public void i_can_go_to_checkout_and_see_all_items() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void i_can_go_to_checkout_and_see_all_items() {
+		 inventoryPage inventoryPage = new inventoryPage(driver);
+    	 inventoryPage.goToCart();
+    	 
+      List<WebElement> results2 = driver.findElements(By.className("cart_item"));
+    	 List<String> names2 = new ArrayList();
+    	 for(WebElement product : results2) {
+    		 //product = driver.findElement(By.className("cart_item"));
+    		 WebElement buttondiv = product.findElement(By.className("cart_item_label"));
+    		 names2.add(buttondiv.findElement(By.className("inventory_item_name")).getText());
+    	 }
+    	
+      assertEquals(names,names2);
 	}
 
 	
@@ -80,8 +119,8 @@ public class StepDefinitions {
 //	    }
 //
 //
-//	    @After
-//	    public void tearDown() {
-//	        driver.close();
-//	    }
+	    @After
+	    public void tearDown() {
+	        driver.close();
+	    }
 }
