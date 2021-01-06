6046f38e07d535625deb8c0eb64007b664bc3273
harryfresco <harry.fresco@students.plymouth.ac.uk>
2021-01-06 12:29:16 +0000

    HF: Added cucumber to Stock test

--- seleniumTest3/src/test/java/seleniumTest/DemoSiteTest.java
+++ seleniumTest3/src/test/java/seleniumTest/DemoSiteTest.java
@@ -1,51 +1,12 @@
 package seleniumTest;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.runner.RunWith;
 
-import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/cuke/DemoSite.feature", monochrome=true,glue = {"stepdef"})
 public class DemoSiteTest {
-	 private WebDriver driver;
-
-	    @Before
-	    public void setup() {
-	        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-	        driver = new ChromeDriver();
-	        driver.manage().window().setSize(new Dimension(1366, 768));
-
-	    }
-
-	    @Test
-	    public void test() throws InterruptedException {
-	        driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100");
-
-	        WebElement cookies = driver.findElement(By.id("acceptCookie"));
-	        cookies.click();
-	        WebElement risers = driver.findElement(By.xpath("//*[@id=\"view-constituents\"]/ul/li[2]/a"));
-	        risers.click();
-	        
-	        WebElement topRiser = driver.findElement(By.xpath("/html/body/main/div/div/div[3]/div[4]/div[1]/div[2]/table/tbody/tr[1]/td[2]"));;  
-	        String riser = topRiser.getText();
-	        WebElement fallers = driver.findElement(By.xpath("/html/body/main/div/div/div[3]/div[4]/div[1]/div[1]/ul/li[3]/a"));; 
-	        fallers.click();
-	        WebElement topFaller = driver.findElement(By.xpath("/html/body/main/div/div/div[3]/div[4]/div[1]/div[2]/table/tbody/tr[1]/td[2]"));;  
-	        String faller = topFaller.getText();
-	        
-	       System.out.println(riser);
-	       System.out.println(faller);
-	    }
-
-	    @After
-	    public void tearDown() {
-	        driver.close();
-	    }
+	
 }
---
+++ seleniumTest3/src/test/resources/cuke/DemoSite.feature
@@ -0,0 +1,11 @@
+Feature: Finding riser and faller
+
+Scenario: Finding riser 
+	Given the correct web address 
+	When I navigate to risers
+	Then I can see the top riser
+
+Scenario: Finding Faller
+	Given the correct web address 
+	When I navigate to fallers
+	Then I can see the top faller
